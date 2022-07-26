bcc2e414839cf7c2cd95415a976fd2a67f624d1c
abinet1 <abinettassewabi@gmail.com>
2022-07-26 09:51:16 +0300

    commit

--- src/components/About/Work.js
+++ src/components/About/Work.js
@@ -1,35 +1,47 @@
+import "./Work.css";
 const Work=()=>{
     return(
         <section className="works">
-            <h2>
+            <h2 className="work_title">
                 <a>Recent Works</a>
             </h2>
-            <ul>
+            <ul className="work_ul">
                 <li>
-                    <h4>Junior Web Developer</h4>
-                    <small>
-                        <span>
-                            Internship In BTG |
-                            <time>
-                                <span> July.14.2022</span>
-                            </time>
-                        </span>                        
-                        <br/>
-                        Took part in 8-monthlong web based system development Internship,working with: JS, HTML, CSS, and django(python) as backend.
-                    </small>
-                </li>
-                <hr />
-                <li>
-                    <h4>Junior Web Developer</h4>
+                    <h4 className="work_title">Junior Web Developer</h4>
                     <small>
                         <span>
                             Jobs In EBC |
                             <time>
-                                <span> July.14.2022</span>
+                                <span> July.14.2022 </span> 
                             </time>
+                             | on going
                         </span>                        
-                        <br/>
-                        I am Tooking part in 2-monthlong web based system development, working with: JS, HTML, CSS, and laravel(php) as backend.
+                    </small>
+                </li>
+                <hr />
+                <li>
+                    <h4 className="work_title">Personal Web Developer Project</h4>
+                    <small>
+                        <span>
+                            Jobs In EBC |
+                            <time>
+                                <span> July.14.2022 </span>
+                            </time>
+                            | on going
+                        </span>                        
+                    </small>
+                </li>
+                <hr />
+                <li>
+                    <h4 className="work_title">Junior Web Developer</h4>
+                    <small>
+                        <span>
+                            Internship In BTG |
+                            <time>
+                                <span> September.5.2021 </span>
+                            </time>
+                            | 8 mounth
+                        </span>                        
                     </small>
                 </li>
             </ul>
--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -8,21 +8,42 @@ const Skill=()=>{
         <section className="skill">
             <Container className="skill-container">
                 <Row >
-                    <Col sm className="divs">    
-                        <h2>Mobile-friendly</h2>
-                        <p>A responsive design makes your website accessible to all users, regardless of their device.</p>
+                    <Col md className="divs">    
+                        <h2>Back-end Web Development</h2>
+                        <br />
+                        <p>Python is my language of choice. The majority of my back-end development work are done in Python.<br />
+                            I use Django, Django rest framework, and Flask as back-end frameworks.<br />
+                            I'm studying Node and Express to expand to my current skill set.
+                        </p>
+                        <p>My goal is to work as a professional back-end developer.</p>
                     </Col>
-                    <Col sm className="divs">    
-                        <h2>Mobile-friendly</h2>
-                        <p>A responsive design makes your website accessible to all users, regardless of their device.</p>
+                    <Col md className="divs">    
+                        <h2>Front-end Web Development</h2>
+                        <br />
+                        <p>I'm new to react. However, it is currently one of the finest front-end libraries I have used.<br/>
+                            I use Figma and Adobe XD for designing and am proficient with HTML, CSS, JS, and Bootstrap.
+                        </p>
+                        <p>In front-end, I want to improve to the point where I can showcase my work. I suppose I don't mind working in front-end alongside back-end.</p>
                     </Col>
-                    <Col sm className="divs">    
-                        <h2>Mobile-friendly</h2>
-                        <p>A responsive design makes your website accessible to all users, regardless of their device.</p>
+                </Row>
+                <Row >
+                    <Col md className="divs">    
+                        <h2>Game Development</h2>
+                        <br />
+                        <p>Python is my language of choice for back-end development. <br />
+                            I use Django, the Django rest framework, and Flask as developed back-end frameworks.<br/>
+                            I'm studying Node and Express to expand to my current skill set.
+                        </p>
+                        <p>My goal is to work as a professional back-end developer.</p>
                     </Col>
-                    <Col sm className="divs">    
-                        <h2>Mobile-friendly</h2>
-                        <p>A responsive design makes your website accessible to all users, regardless of their device.</p>
+                    <Col md className="divs">    
+                        <h2>ML and Data Analysis</h2>
+                        <br />
+                        <p>Python is my language of choice for back-end development. <br />
+                            I use Django, the Django rest framework, and Flask as developed back-end frameworks.<br />
+                            I'm studying Node and Express to expand to my current skill set.
+                        </p>
+                        <p>My goal is to work as a professional back-end developer.</p>
                     </Col>
                 </Row>
             </Container>
